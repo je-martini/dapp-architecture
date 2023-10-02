16495fc865ecfacf6f3d435956070414950c052e
je-martini <jesusmartini2017@gmail.com>
2023-10-02 09:18:06 -0400

    fixing bug about the first test it was just the deployed() funcion, and also i made the deployend in the localhost all set and good

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const Crypto_menu = await hre.ethers.deployContract("menu");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await Crypto_menu.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Crypto_menu was deployed to ${Crypto_menu.target}`
   );
 }
 
--- test/first-test.js
+++ test/first-test.js
@@ -10,11 +10,11 @@ describe("crypto_menu", function () {
 
     const [owner, addr1] = await ethers.getSigners();
     
-    const Menu = await ethers.getContractFactory("menu");
-    const menu = await Menu.deploy();
-    await menu.deployed();
+    const Crypto_menu = await ethers.getContractFactory("menu");
+    const crypto_menu = await Crypto_menu.deploy();
+    // await crypto_menu.deployed();
     
-    var addFood = await menu.add_food(
+    var addFood = await crypto_menu.add_food(
       "https",
       "gallo pinto",
       "costa rica"
@@ -22,7 +22,7 @@ describe("crypto_menu", function () {
 
     await addFood.wait();
 
-    var addFood2 = await menu.connect(addr1).add_food(
+    var addFood2 = await crypto_menu.connect(addr1).add_food(
       "https-google",
       "pavellon",
       "venezuela"
@@ -30,7 +30,10 @@ describe("crypto_menu", function () {
 
     await addFood2.wait();
 
-    var foods = await menu.get_all_foods();
+    var foods = await crypto_menu.get_all_foods();
     expect(foods.length).to.equal(2);
+
+    var foods_by_owner = await crypto_menu.get_food_by_owner();
+    expect(foods_by_owner.length).to.equal(1);
   })
 })
